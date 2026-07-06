be47595d237c38b2f668bc7f4af7044041d236bd
Savindu Panagoda <1savindupanagoda@gmail.com>
2026-07-06 11:14:17 +0530

    init

---
+++ task-manager-backend/src/test/java/me/savindu/task_manager_backend/TaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.savindu.task_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
